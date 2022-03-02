ed991afbe63f97c493ec9b094754ee199314f5ca
riezky maisyar <riezkymaisyar10@gmail.com>
2022-03-02 17:03:16 +0700

    menambahkan menambahkan POST addBookHandler pada handler.js

---
+++ src/handler.js
@@ -0,0 +1,81 @@
+/* eslint-disable no-unused-vars */
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+// eslint-disable-next-line consistent-return
+const addBookHandler = (request, h) => {
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
+
+  if (name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+
+    return response;
+  }
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatetedAt = insertedAt;
+
+  if (pageCount < readPage) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+
+    return response;
+  }
+
+  const finished = (pageCount === readPage);
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatetedAt,
+  };
+
+  books.push(newBook);
+
+  const isSucces = books.filter((book) => book.id === id).length > 0;
+
+  if (isSucces) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan',
+  });
+  response.code(500);
+
+  return response;
+};
+
+const getAllBookHandler = () => ({
+
+});
+
+module.exports = { addBookHandler };
---
+++ src/routes.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: () => {},
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: () => {},
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
